2bdffc384fc839ae7da24069aa6c613eba549c31
Piotr Zychlinski <soniapiotr@gmail.com>
2024-07-10 12:36:06 +0100

    Fix jog simulation cancellation logic

--- src/scripts/MachineState.ts
+++ src/scripts/MachineState.ts
@@ -13,7 +13,8 @@ class MachineState {
     motionMode: MotionMode;
     #bufferLineCharCounts: number[];
     #numOfCharsInBuffer: number;
-    #simulationTimeoutId: number;
+    #simulationTimeoutId: number | undefined;
+    #jogSimulationTimeoutId: number | undefined;
     #motionSimulationEnabled: boolean;
     #inMotion: boolean;
     #motionCommandQueue: Uint8Array[];
@@ -89,6 +90,8 @@ class MachineState {
         if (this.#motionSimulationEnabled) {
             this.#inMotion = false;
             this.#motionCommandQueue.length = 0;
+            this.#simulationTimeoutId = undefined;
+            this.#jogSimulationTimeoutId = undefined;
         }
         this.#motionSimulationEnabled = !this.#motionSimulationEnabled;
     }
@@ -124,8 +127,11 @@ class MachineState {
     #registerRealtimeCommand(command: number) {
         switch (command) {
             case GRBL.CANCEL_JOG_COMMAND:
-                this.#inMotion = false;
-                clearTimeout(this.#simulationTimeoutId); // Cancel simulation upon cancelling jog
+                if (this.#jogSimulationTimeoutId != null) {
+                    this.#inMotion = false;
+                    clearTimeout(this.#jogSimulationTimeoutId); // Cancel jog simulation upon cancelling jog
+                    this.#jogSimulationTimeoutId = undefined;
+                }
                 this.#clearQueuedJogCommands();
                 break;
         }
@@ -161,7 +167,7 @@ class MachineState {
         }
     }
 
-    #beginLinearMotionSimulation(motionVec: Partial<Vec3>, feedrate?: number, distanceMode?: DistanceMode) {
+    #beginLinearMotionSimulation(motionVec: Partial<Vec3>, isJog = false, feedrate?: number, distanceMode?: DistanceMode) {
         this.#inMotion = true;
         feedrate ??= this.feedRate;
         distanceMode ??= this.distanceMode;
@@ -180,27 +186,33 @@ class MachineState {
             const axesFeedrate = { x: (motionVec.x / totalDistance) * feedratePerMs, y: (motionVec.y / totalDistance) * feedratePerMs, z: (motionVec.z / totalDistance) * feedratePerMs };
             const stepVec = { x: axesFeedrate.x * SIMULATION_UPDATE_INTERVAL_MS, y: axesFeedrate.y * SIMULATION_UPDATE_INTERVAL_MS, z: axesFeedrate.z * SIMULATION_UPDATE_INTERVAL_MS };
             const stepDistance = Math.sqrt(stepVec.x ** 2 + stepVec.y ** 2 + stepVec.z ** 2);
-            this.#simulateLinearMotion({ ...this.pos }, stepVec, stepDistance, totalDistance, 0);
+            this.#simulateLinearMotion({ ...this.pos }, stepVec, stepDistance, totalDistance, 0, isJog);
         }
     }
 
-    #simulateLinearMotion(startVec: Vec3, stepVec: Vec3, stepDistance: number, totalDistance: number, distanceTravelled: number) {
-        this.#simulationTimeoutId = setTimeout(() => {
-            this.pos.x += stepVec.x;
-            this.pos.y += stepVec.y;
-            this.pos.z += stepVec.z;
-            distanceTravelled = Math.sqrt((this.pos.x - startVec.x) ** 2 + (this.pos.y - startVec.y) ** 2 + (this.pos.z - startVec.z) ** 2);
-            if (this.#motionSimulationEnabled) {
-                if (distanceTravelled < (totalDistance - stepDistance)) {
-                    this.#simulateLinearMotion(startVec, stepVec, stepDistance, totalDistance, distanceTravelled);
-                } else {
-                    this.#inMotion = false;
-                    if (this.#motionCommandQueue.length > 0) {
-                        this.registerCommand(this.#motionCommandQueue.shift()!);
-                    }
-                }
+    #simulateLinearMotion(startVec: Vec3, stepVec: Vec3, stepDistance: number, totalDistance: number, distanceTravelled: number, isJog: boolean) {
+        this.pos.x += stepVec.x;
+        this.pos.y += stepVec.y;
+        this.pos.z += stepVec.z;
+        distanceTravelled = Math.sqrt((this.pos.x - startVec.x) ** 2 + (this.pos.y - startVec.y) ** 2 + (this.pos.z - startVec.z) ** 2);
+        if (this.#motionSimulationEnabled) {
+            if (distanceTravelled < (totalDistance - stepDistance)) {
+                const timeoutId = setTimeout(() =>
+                    this.#simulateLinearMotion(startVec, stepVec, stepDistance, totalDistance, distanceTravelled, isJog),
+                    SIMULATION_UPDATE_INTERVAL_MS);
+                this.#setSimulationTimeoutId(timeoutId, isJog);
+            } else {
+                this.#endMotion();
             }
-        }, SIMULATION_UPDATE_INTERVAL_MS);
+        }
+    }
+
+    #setSimulationTimeoutId(timeoutId: number, isJog = false) {
+        if (isJog) {
+            this.#jogSimulationTimeoutId = timeoutId;
+        } else {
+            this.#simulationTimeoutId = timeoutId;
+        }
     }
 
     // Reference: https://github.com/gnea/grbl/wiki/Grbl-v1.1-Jogging
@@ -238,7 +250,7 @@ class MachineState {
                 if (this.#inMotion) {
                     this.#motionCommandQueue.push(command);
                 } else {
-                    this.#beginLinearMotionSimulation(jogMotionVec, jogFeedRate, jogDistanceMode);
+                    this.#beginLinearMotionSimulation(jogMotionVec, true, jogFeedRate, jogDistanceMode);
                 }
             }
         } catch (e) {
@@ -255,6 +267,15 @@ class MachineState {
         const textDecoder = new TextDecoder();
         this.#motionCommandQueue = this.#motionCommandQueue.filter((command) =>
             !textDecoder.decode(command).startsWith(GRBL.JOG_COMMAND_PREFIX));
+    }
+
+    #endMotion() {
+        this.#inMotion = false;
+        if (this.#motionCommandQueue.length > 0) {
+            this.registerCommand(this.#motionCommandQueue.shift()!);
+        }
+        this.#simulationTimeoutId = undefined;
+        this.#jogSimulationTimeoutId = undefined;
     }
 }
 
